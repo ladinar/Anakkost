a6648abe242309938b117f9303979c9ea31a1d41
Ladinar Nanda Aprilia <ladinarnanda@gmail.com>
2016-09-10 07:42:46 -0700

    Radiobutton

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1016/anakkost/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1016/anakkost/MainActivity.java
@@ -4,16 +4,21 @@ package id.sch.smktelkom_mlg.tugas01.xirpl1016.anakkost;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etnama;
+    EditText etalamat;
     Spinner spmakanan;
     TextView tvHasil;
-    Button
+    RadioButton delivery, cash;
+    RadioGroup rgStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +27,42 @@ public class MainActivity extends AppCompatActivity {
 
         spmakanan = (Spinner) findViewById(R.id.makanan);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        etnama = (EditText) findViewById(R.id.pemesan);
+        etalamat = (EditText) findViewById(R.id.alamat);
+        /*delivery= (RadioButton) findViewById(R.id.delivery);
+        cash= (RadioButton) findViewById(R.id.cash);*/
 
         findViewById(R.id.buttonPesan).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doClick();
+                doProcess();
             }
         });
     }
 
-    private void doClick() {
-        tvHasil.setText("Terimakasih Telah memesan" + spmakanan.getSelectedItem().toString() + "di Kriyuk Syek");
+    private void doProcess() {
+        if (isvalid()) {
+            String nama = etnama.getText().toString();
+            tvHasil.setText("Terimakasih " + etnama + " Telah memesan " + spmakanan.getSelectedItem().toString() + " di Kriyuk Syek");
+        }
     }
+
+    private boolean isvalid() {
+        boolean valid = true;
+
+        String nama = etnama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etnama.setError("Nama Belum diisi");
+            valid = false;
+        } else {
+            etnama.setError(null);
+        }
+        return valid;
+    }
+
+
+
 }
 
 
