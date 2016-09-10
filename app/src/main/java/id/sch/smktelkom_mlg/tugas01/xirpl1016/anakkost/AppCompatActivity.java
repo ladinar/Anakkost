d02f79b442525d7570f9c82d771cb2beb2dfee0a
Ladinar Nanda Aprilia <ladinarnanda@gmail.com>
2016-09-10 14:48:32 -0700

    Mengalikan jumlah pesanan dengan harga

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1016/anakkost/AppCompatActivity.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl1016.anakkost;
+
+/**
+ * Created by Smktelkom on 9/10/2016.
+ */
+public class AppCompatActivity {
+
+}
--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1016/anakkost/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1016/anakkost/MainActivity.java
@@ -1,57 +1,39 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl1016.anakkost;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    CheckBox cbpcl, cbllp, cbbbr, cbjer, cbteh, cbkop;
-    EditText etNama;
-    Button bpesan;
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spmakanan;
     TextView tvHasil;
-    int nmakan;
+    Button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cbpcl = (CheckBox) findViewById(R.id.checkBoxpcl);
-        cbllp = (CheckBox) findViewById(R.id.checkBoxllp);
-        cbbbr = (CheckBox) findViewById(R.id.checkBoxbbr);
-        cbjer = (CheckBox) findViewById(R.id.checkBoxjer);
-        cbteh = (CheckBox) findViewById(R.id.checkBoxteh);
-        cbkop = (CheckBox) findViewById(R.id.checkBoxkop);
-
-        etNama = (EditText) findViewById(R.id.editTextNama);
-        bpesan = (Button) findViewById(R.id.buttonPesan);
+        spmakanan = (Spinner) findViewById(R.id.makanan);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
-        cbpcl.setOnCheckedChangeListener(this);
-        cbllp.setOnCheckedChangeListener(this);
-        cbbbr.setOnCheckedChangeListener(this);
-        cbjer.setOnCheckedChangeListener(this);
-        cbteh.setOnCheckedChangeListener(this);
-        cbkop.setOnCheckedChangeListener(this);
-
-        bpesan.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.buttonPesan).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                String nama = etNama.getText().toString();
-                tvHasil.setText(nama);
+            public void onClick(View v) {
+                doClick();
             }
         });
     }
 
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) nmakan;
+    private void doClick() {
+        tvHasil.setText("Terimakasih Telah memesan" + spmakanan.getSelectedItem().toString() + "di Kriyuk Syek");
     }
 }
+
+
